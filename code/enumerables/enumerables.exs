4f86cce0361f427fe9560527f48e264680f078cf
Karmen Blake <dudeblake@gmail.com>
2016-04-15 18:50:43 -0700

    add piping enumerables. Streams

--- code/enumerables/enumerables.exs
+++ code/enumerables/enumerables.exs
@@ -14,6 +14,32 @@ IO.inspect Enum.reduce(nums, fn(num, acc) -> num + acc end)
 IO.inspect Enum.filter(nums, &(rem(&1,2) == 0))
 # IO.inspect nums |> Enum.filter &(rem(&1,2) == 0)
 
+
+
+
+
+require Integer
+
+# combine via piping
+# first argument of function is the result of the previous call
+nums = 1..100
+nums
+|> Enum.map(&(&1 * 5))                        # muliply all nums by 5
+|> Enum.filter(&Integer.is_even/1)            # filter for those that are even
+|> Enum.reduce(fn num, acc -> num + acc end)  # reduce by summing
+
+
+
+# loads 10 million integers up front then takes 5
+Enum.map(1..10_000_000, &(&1+1)) |> Enum.take(5)
+
+
+# Streams and lazy loading
+# take five up front and finish. no more processing
+Stream.map(1..10_000_000, &(&1+1)) |> Enum.take(5)
+
+
+
 # More fun
 # http://elixir-lang.org/docs/stable/elixir/Enum.html
 :ok
